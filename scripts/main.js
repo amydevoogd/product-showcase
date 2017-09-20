dc84c696705d027762316a47e249297ba520da03
Amy <amy@devoogd.com>
2017-09-20 17:03:22 -0500

    Scrollmagic completed

--- scripts/main.js
+++ scripts/main.js
@@ -1,85 +1,60 @@
 // init ScrollMagic Controller
 var controller = new ScrollMagic.Controller();
 
-// Scene handler for tweening bar
-var barTween = new ScrollMagic.Scene({
-  triggerElement: ".heroLaptop",
-})
-.setTween(".heroBar", 2, {scale: 0.45, ease:Bounce.easeOut}) // trigger a TweenMax.to tween
-.addIndicators({name: "scale bar"})
 
-
-// Scene handlers for layers
-var pinHeroElements = new ScrollMagic.Scene({
-  triggerElement: ".heroPin", // point of execution
+// Scene handler for pinned scene
+var pinHeroWrapper = new ScrollMagic.Scene({
+  triggerElement: ".hero", // point of execution
   triggerHook: 0, // don't trigger until #heroHead hits the top of the viewport
+  duration: 500,
   reverse: true // allows the effect to trigger when scrolled in the reverse direction
 })
-.setPin(".heroPin", {pushfollowers: false}) // the element we want to pin
-.addIndicators({
-  name: "pin hero elements",
-  colorTrigger: "green",
-  colorStart: "green"
+.setPin(".hero-wrapper") // the element we want to pin
+// enable for work in progress
+/*.addIndicators({
+  name: "pin hero",
+  colorTrigger: "magenta",
+  colorStart: "magenta"
 })
-
-var pinLaptop = new ScrollMagic.Scene({
-  triggerElement: ".heroLaptop",
-  triggerHook: 0.07,
-  reverse: true
-})
-.setPin(".heroLaptop")
-.addIndicators({
-  name: "pin laptop",
-  colorTrigger: "red",
-  colorStart: "red"
-})
+*/
+.addTo(controller);
 
 
-var pinNotebook = new ScrollMagic.Scene({
-  triggerElement: ".heroNotebook",
-  triggerHook: 0.45,
-  reverse: true
-})
-.setPin(".heroNotebook")
-.addIndicators({
-  name: "pin notebook",
-  colorTrigger: "blue",
-  colorStart: "blue"
-})
+// TweenMax.for tweening bar from small to large
+var tween = TweenMax.from('.hero-bar', 1, {scale: .45});
 
 
-var pinGlasses = new ScrollMagic.Scene({
-  triggerElement: ".heroGlasses",
-  triggerHook: 0.47,
-  reverse: true
+// Scene handler for tweening bar
+var barTween = new ScrollMagic.Scene({
+  duration: 800
 })
-.setPin(".heroGlasses")
-.addIndicators({
-  name: "pin glasses",
-  colorTrigger: "chocolate",
-  colorStart: "chocolate"
-})
+.setTween(tween) // trigger above TweenMax.for tween
+//.addIndicators({name: "scale bar"})
+.addTo(controller);
 
 
-var pinTabletop = new ScrollMagic.Scene({
-  triggerElement: ".heroTabletop",
-  triggerHook: 0.43,
-  reverse: true
-})
-.setPin(".heroTabletop")
-.addIndicators({
-  name: "pin tabletop",
-  colorTrigger: "BlueViolet",
-  colorStart: "BlueViolet"
-})
+// disable Scrollmagic at smaller sizes
+controller.scrollPos(function () {
+  if(window.innerWidth >= 768){
+    return window.pageYOffset;
+  } else {
+    return 0;
+  }
+});
 
+// change testimonial when logo is clicked
+$(document).ready(function(){
+  
+  $("logo01").click(function() {
+    $("testimonial-quotes").html("Engadget likes it");
+  });
 
-// Add Scenes to ScrollMagic Controller
-controller.addScene([
-  barTween,
-  pinHeroElements,
-  pinLaptop,
-  pinNotebook,
-  pinGlasses,
-  pinTabletop
-]);
+  $("logo02").click(function() {
+    $("testimonial-quotes").html("Ars Technica likes it");
+  });
+
+  $("logo03").click(function() {
+    $("testimonial-quotes").html("Medium likes it");
+  });
+
+});
